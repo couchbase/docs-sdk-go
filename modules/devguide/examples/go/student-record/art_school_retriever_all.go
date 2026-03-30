400acec07cf965be02a37a5bd390c4cbd50aec6a
Charles Dixon <chvckd@gmail.com>
2026-03-30 15:10:36 +0100

    GOCBC-1795: Add student record tutorial code

---
+++ modules/devguide/examples/go/student-record/art_school_retriever_all.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/couchbase/gocb/v2"
+)
+
+func main() {
+	connectionString := "<<connection-string>>" // Replace this with Connection String
+	username := "<<username>>"                   // Replace this with username from cluster access credentials
+	password := "<<password>>"                   // Replace this with password from cluster access credentials
+
+	options := gocb.ClusterOptions{
+		Authenticator: gocb.PasswordAuthenticator{
+			Username: username,
+			Password: password,
+		},
+	}
+
+	if err := options.ApplyProfile(gocb.ClusterConfigProfileWanDevelopment); err != nil {
+		log.Fatal(err)
+	}
+
+	cluster, err := gocb.Connect("couchbases://"+connectionString, options)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	retrieveCourses(cluster)
+
+	err = cluster.Close(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func retrieveCourses(cluster *gocb.Cluster) {
+	queryResult, err := cluster.Query(
+		"SELECT crc.* FROM `student-bucket`.`art-school-scope`.`course-record-collection` crc",
+		&gocb.QueryOptions{},
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for queryResult.Next() {
+		var row interface{}
+		err := queryResult.Row(&row)
+		if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Println("Found row:", row)
+	}
+
+	if err := queryResult.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
---
+++ modules/devguide/examples/go/student-record/insert_courses.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"log"
+	"time"
+
+	"github.com/couchbase/gocb/v2"
+)
+
+func main() {
+	connectionString := "<<connection-string>>" // Replace this with Connection String
+	username := "<<username>>"                   // Replace this with username from cluster access credentials
+	password := "<<password>>"                   // Replace this with password from cluster access credentials
+
+	options := gocb.ClusterOptions{
+		Authenticator: gocb.PasswordAuthenticator{
+			Username: username,
+			Password: password,
+		},
+	}
+
+	if err := options.ApplyProfile(gocb.ClusterConfigProfileWanDevelopment); err != nil {
+		log.Fatal(err)
+	}
+
+	cluster, err := gocb.Connect("couchbases://"+connectionString, options)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	bucket := cluster.Bucket("student-bucket")
+	err = bucket.WaitUntilReady(10*time.Second, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	scope := bucket.Scope("art-school-scope")
+
+	// The code here is similar to creating a student record, but it writes to a different collection.
+	courseRecords := scope.Collection("course-record-collection")
+
+	addCourse(courseRecords, "ART-HISTORY-000001", "art history", "fine art", 100)
+	addCourse(courseRecords, "FINE-ART-000002", "fine art", "fine art", 50)
+	addCourse(courseRecords, "GRAPHIC-DESIGN-000003", "graphic design", "media and communication", 200)
+
+	err = cluster.Close(nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func addCourse(collection *gocb.Collection, id, name, faculty string, creditPoints int) {
+	course := map[string]interface{}{
+		"course-name":   name,
+		"faculty":       faculty,
+		"credit-points": creditPoints,
+	}
+
+	_, err := collection.Upsert(id, course, nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
