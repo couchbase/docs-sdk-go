b4583204ceadbbfe375ca8f706b6b6fc5fa9235c
Richard Smedley <rgsmedley@gmail.com>
2026-09-23 13:24:03 +0100

    Add bulk_get example for Couchbase Go SDK

---
+++ modules/devguide/examples/go/bulk_get.go
@@ -0,0 +1,97 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	gocb "github.com/couchbase/gocb/v2"
+)
+
+func Example_concurrentGet() {
+	// #tag::connect[]
+	opts := gocb.ClusterOptions{
+		Authenticator: gocb.PasswordAuthenticator{
+			Username: "Administrator",
+			Password: "password",
+		},
+	}
+	cluster, err := gocb.Connect("localhost", opts)
+	if err != nil {
+		panic(err)
+	}
+
+	bucket := cluster.Bucket("travel-sample")
+	collection := bucket.Scope("inventory").Collection("airline")
+
+	// We wait until the bucket is connected and setup.
+	err = bucket.WaitUntilReady(5*time.Second, nil)
+	if err != nil {
+		panic(err)
+	}
+	// #end::connect[]
+
+	// #tag::prepareOps[]
+	// Replace with your actual document IDs.
+	docIDs := []string{
+		"airline_10",
+		"airline_10123",
+		"airline_10226",
+		"airline_10642",
+		"airline_10748",
+		"airline_10765",
+		"airline_109",
+		"airline_792",
+		"airline_8745",
+		"airline_8809",
+		"airline_9833",
+	}
+
+  	// #tag::bulk-get[]
+	var getOps []gocb.BulkOp
+	for _, id := range docIDs {
+		getOps = append(getOps, &gocb.GetOp{
+			ID: id,
+		})
+	}
+	// #end::bulk-get[]
+  // #end::prepareOps[]
+
+	// #tag::send[]
+	startTime := time.Now()
+
+	err = collection.Do(getOps, nil)
+	if err != nil {
+		log.Println(err)
+	}
+
+	// Be sure to check each individual operation for errors too.
+	var fetchedCount int
+	for _, op := range getOps {
+		getOp, ok := op.(*gocb.GetOp)
+		if !ok {
+			log.Println("failed to type assert BulkOp to *gocb.GetOp")
+			continue
+		}
+
+		if getOp.Err != nil {
+			log.Printf("failed to fetch document %s: %v\n", getOp.ID, getOp.Err)
+			continue
+		}
+
+		var docContent interface{}
+		if err := getOp.Result.Content(&docContent); err != nil {
+			log.Printf("failed to decode document %s content: %v\n", getOp.ID, err)
+			continue
+		}
+
+		fetchedCount++
+		fmt.Printf("Fetched document #%d: ID: %s, Content: %v\n", fetchedCount, getOp.ID, docContent)
+	}
+
+	duration := time.Since(startTime)
+	log.Printf("Completed fetching %d/%d documents in %d milliseconds\n", fetchedCount, len(docIDs), duration.Milliseconds())
+
+	cluster.Close(nil)
+	// #end::send[]
+}
